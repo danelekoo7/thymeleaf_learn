f0acb6118d7c1d911091aee48b234d229e9db374
danelekoo7 <danieljedrus@gmail.com>
2020-05-25 14:26:13 +0200

    * form

--- src/main/java/pl/jedrus/thymeleaflearn/controller/CarController.java
+++ src/main/java/pl/jedrus/thymeleaflearn/controller/CarController.java
@@ -3,26 +3,43 @@ package pl.jedrus.thymeleaflearn.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.jedrus.thymeleaflearn.pojo.Car;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    @GetMapping("hello")
-    public String get(Model model) {
+    private List<Car> cars;
+
+    public CarController() {
         Car car1 = new Car("Citigo", "Skoda");
         Car car2 = new Car("tipo", "Fiat");
         Car car3 = new Car("e46", "BMW");
         Car car4 = new Car("golf", "VW");
+        cars = new ArrayList<>();
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
+    }
 
-        List<Car> cars = Arrays.asList(car1, car2, car3, car4);
+    @GetMapping("/car")
+    public String get(Model model) {
+
 
         model.addAttribute("cars", cars);
-        model.addAttribute("car", car1);
+        model.addAttribute("newCar", new Car());
         return "car";
+    }
 
+    @PostMapping("/add-car")
+    public String addCar(@ModelAttribute Car car) {
+        cars.add(car);
+        return "redirect:/car";
     }
 }
--- src/main/java/pl/jedrus/thymeleaflearn/controller/HelloController.java
+++ src/main/java/pl/jedrus/thymeleaflearn/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("car")
+    @GetMapping("hello")
     public String getCar(Model model) {
 
         model.addAttribute("name", "Daniel");
